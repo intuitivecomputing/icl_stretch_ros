b2211bd22a03dd0b645b2dd537d1e82ed004bb71
Yuxiang Gao <yuxiang.gao@jhu.edu>
2021-10-27 19:09:30 -0400

    Add magnet trigger

--- stretch_electromagnet/src/main.cpp
+++ stretch_electromagnet/src/main.cpp
@@ -4,36 +4,47 @@
 
 #include <ros.h>
 #include <std_srvs/SetBool.h>
+#include <std_srvs/Trigger.h>
 
 ros::NodeHandle nh;
 using std_srvs::SetBool;
+using std_srvs::Trigger;
 
-int pin = 3;
+int PIN = 3;
 void callback(const SetBool::Request &req, SetBool::Response &res)
 {
   if (req.data)
   {
-    digitalWrite(pin, HIGH);
+    digitalWrite(PIN, HIGH);
     res.message = "magnet ON";
   }
   else
   {
-    digitalWrite(pin, LOW);
+    digitalWrite(PIN, LOW);
     res.message = "magnet OFF";
   }
   res.success = true;
 }
 
+void triggerCallback(const Trigger::Request &req, Trigger::Response &res)
+{
+  digitalWrite(PIN, digitalRead(PIN) == HIGH ? LOW : HIGH);
+  res.success = true;
+}
+
 ros::ServiceServer<SetBool::Request, SetBool::Response> server("magnet", &callback);
+ros::ServiceServer<Trigger::Request, Trigger::Response> server_trigger("magnet_toggle", &triggerCallback);
+
 
 void setup()
 {
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, LOW);
+  pinMode(PIN, OUTPUT);
+  digitalWrite(PIN, LOW);
 
   nh.getHardware()->setBaud(57600);
   nh.initNode();
   nh.advertiseService(server);
+  nh.advertiseService(server_trigger);
 }
 
 void loop()
